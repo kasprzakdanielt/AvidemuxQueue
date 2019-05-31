c49542dd03c2731d6cdd8a3d5f3fbafc2eeb0c11
Daniel <kasprzak.szkola@gmail.com>
2019-05-31 11:49:46 +0200

    added separate class to handle files and moved few functions, few left to move

--- src/main/java/fqt/Controller.java
+++ src/main/java/fqt/Controller.java
@@ -1,6 +1,5 @@
 package fqt;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,13 +10,12 @@ import javafx.scene.input.TransferMode;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
+
+import static fqt.FileHandler.Start_conversion;
+import static fqt.FileHandler.ffmpeg_get_codecs;
 
 
 public class Controller implements Initializable {
@@ -26,13 +24,8 @@ public class Controller implements Initializable {
     public Button add_button;
     public TextField output_path;
     public ProgressBar progress_bar;
-    private String output_audiocodec = "";
-    private String filepath = "";
-    private String filename = "";
-    private Double done_files = 0.0;
-    private String ffprobe_path = System.getProperty("user.dir") + "/ffmpeg-4.1.3-win64-static/bin/ffprobe.exe";
-    private String ffmpeg_path = System.getProperty("user.dir") + "/ffmpeg-4.1.3-win64-static/bin/ffmpeg.exe";
 
+    private String output_audiocodec = "";
 
     @FXML
     private ComboBox<String> audiocodec_combobox;
@@ -45,7 +38,6 @@ public class Controller implements Initializable {
     @FXML
     TableColumn<TableModel, String> status_column;
 
-    private ObservableList<TableModel> tableData = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -73,65 +65,15 @@ public class Controller implements Initializable {
 
     @FXML
     private void start_pressed() {
-        if (output_audiocodec.equals("")) {
-            warning_dialog("Output codec is not selected");
-        }
-        if (output_path.getText().isEmpty()) {
-            warning_dialog("Destination was not specified");
-        } else {
-            new Thread(() -> {
-                progress_bar.setProgress(0.0);
-                done_files = 0.0;
-                Integer table_size = tableData.size();
-                tableData.forEach((temp) -> {
-                    filename = temp.getFileName();
-                    filepath = temp.getFilepath();
-                    String destination = output_path.getText() + "\\" + filename;
-                    temp.setStatus(start_conversion(destination, filepath));
-                    fileList.refresh();
-                    progress(table_size);
-                });
-            }).start();
-        }
-    }
-
-    private void progress(Integer table_size) {
-        done_files += 1.0;
-        progress_bar.setProgress((done_files / table_size));
-    }
-
-    private String start_conversion(String destination, String filepath) {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        String query = String.format("%s -loglevel error -i %s -c:v copy -c:a %s %s", ffmpeg_path, filepath, output_audiocodec, destination);
-        System.out.println(query);
-        try {
-            processBuilder.command("cmd.exe", "/c", query);
-            Process p = processBuilder.start();
-            p.waitFor();
-            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            if (input.readLine() != null) {
-                return "Error";
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "Success";
-    }
-
-    private void warning_dialog(String message) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Careful");
-        alert.setHeaderText("User error");
-        alert.setContentText(message);
-        alert.showAndWait();
+        Start_conversion(fileList, output_audiocodec, output_path.getText(), progress_bar);
     }
 
     @FXML
     private void handleDragOver(DragEvent event) {
         if (event.getDragboard().hasFiles()) {
             final Boolean[] allow_drop = {true};
-            event.getDragboard().getFiles().forEach(item ->{
-                if(!item.getName().toLowerCase().endsWith(".mkv")){
+            event.getDragboard().getFiles().forEach(item -> {
+                if (!item.getName().toLowerCase().endsWith(".mkv")) {
                     allow_drop[0] = false;
                 }
             });
@@ -143,23 +85,6 @@ public class Controller implements Initializable {
 
     @FXML
     private void handleDrop(DragEvent event) {
-        List<File> files = event.getDragboard().getFiles();
-        new Thread(() -> {
-            files.forEach(file -> {
-                String audiocodec = "Error";
-                filepath = file.getAbsolutePath();
-                String query = String.format("%s -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 %s", ffprobe_path, files.get(0).getAbsolutePath());
-                try {
-                    Process p = Runtime.getRuntime().exec(query);
-                    BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    audiocodec = input.readLine();
-                    filename = file.getName();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                tableData.add(new TableModel(filename, audiocodec, "Waiting", filepath));
-            });
-        }).start();
-        fileList.setItems(tableData);
+        fileList.setItems(ffmpeg_get_codecs(event));
     }
 }
---
+++ src/main/java/fqt/FileHandler.java
@@ -0,0 +1,99 @@
+package fqt;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableView;
+import javafx.scene.input.DragEvent;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static fqt.DialogsHandler.warning_dialog;
+
+class FileHandler {
+    private static String ffprobe_path = System.getProperty("user.dir") + "/ffmpeg-4.1.3-win64-static/bin/ffprobe.exe";
+    private static String ffmpeg_path = System.getProperty("user.dir") + "/ffmpeg-4.1.3-win64-static/bin/ffmpeg.exe";
+    private static double done_files = 0.0;
+    static ObservableList<TableModel> tableData = FXCollections.observableArrayList();
+
+
+    static ObservableList<TableModel> ffmpeg_get_codecs(DragEvent event){
+        List<File> files = event.getDragboard().getFiles();
+
+        new Thread(() -> {
+            files.forEach(file -> {
+                String filepath = file.getAbsolutePath();
+                String query = String.format("%s -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 %s", ffprobe_path, files.get(0).getAbsolutePath());
+                try {
+                    Process p = Runtime.getRuntime().exec(query);
+                    BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String audiocodec = input.readLine();
+                    String filename = file.getName();
+                    tableData.add(new TableModel(filename, audiocodec, "Waiting", filepath));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            });
+        }).start();
+        return tableData;
+
+    }
+
+
+    static void Start_conversion(TableView<TableModel> fileList, String output_audiocodec, String output_path, ProgressBar progress_bar){
+        if (output_audiocodec.equals("")) {
+            warning_dialog("Output codec is not selected");
+        }
+        if (output_path.isEmpty()) {
+            warning_dialog("Destination was not specified");
+        } else {
+            progress_bar.setProgress(0.0);
+
+            new Thread(() -> {
+                done_files = 0.0;
+                Integer table_size = FileHandler.tableData.size();
+                FileHandler.tableData.forEach((temp) -> {
+                    String filename = temp.getFileName();
+                    String filepath = temp.getFilepath();
+                    String destination = output_path + "\\" + filename;
+                    temp.setStatus(start_conversion(destination, filepath, output_audiocodec));
+                    fileList.refresh();
+                    progress(table_size, progress_bar);
+                });
+            }).start();
+
+        }
+
+
+    }
+
+    static void progress(Integer table_size, ProgressBar progress_bar) {
+        done_files += 1.0;
+        progress_bar.setProgress((done_files / table_size));
+    }
+
+    static private String start_conversion(String destination, String filepath, String output_audiocodec) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String query = String.format("%s -loglevel error -i %s -c:v copy -c:a %s %s", ffmpeg_path, filepath, output_audiocodec, destination);
+        System.out.println(query);
+        try {
+            processBuilder.command("cmd.exe", "/c", query);
+            Process p = processBuilder.start();
+            p.waitFor();
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            if (input.readLine() != null) {
+                return "Error";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Success";
+    }
+
+
+}
